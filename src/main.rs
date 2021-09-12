8b3fd5a3c5ab068afefe8bb4c7d9726697ef8a5f
RayWiis <raywiis@live.com>
2021-09-12 21:09:18 +0300

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,16 @@ struct Solution {}
 impl Solution {
     pub fn find_best_value(arr: Vec<i32>, target: i32) -> i32 {
         let mut res = 0;
-
         let mut new_distance = i32::MAX - 1;
         let mut old_distance = i32::MAX;
 
         while new_distance < old_distance {
-            let result: i32= arr.iter()
-                .map(|num| if *num > res {
-                    res
-                } else {
-                    *num
-                })
+            let result: i32 = arr
+                .iter()
+                .map(|num| if *num > res { res } else { *num })
                 .sum();
-
             old_distance = new_distance;
             new_distance = (target - result).abs();
-
             res += 1;
         }
 
@@ -27,7 +21,7 @@ impl Solution {
 }
 
 fn main() {
-    let arr = vec!(4, 5, 9);
+    let arr = vec![4, 5, 9];
     let target = 10;
 
     let res = Solution::find_best_value(arr.clone(), target);
@@ -40,7 +34,7 @@ mod tests {
 
     #[test]
     fn example_1() {
-        let arr = vec!(4,9,3);
+        let arr = vec![4, 9, 3];
         let target = 10;
         let res = Solution::find_best_value(arr, target);
         assert_eq!(res, 3);
@@ -48,7 +42,7 @@ mod tests {
 
     #[test]
     fn example_2() {
-        let arr = vec!(2,3,5);
+        let arr = vec![2, 3, 5];
         let target = 10;
         let res = Solution::find_best_value(arr, target);
         assert_eq!(res, 5);
@@ -56,7 +50,7 @@ mod tests {
 
     #[test]
     fn example_3() {
-        let arr = vec!(60864,25176,27249,21296,20204);
+        let arr = vec![60864, 25176, 27249, 21296, 20204];
         let target = 56803;
         let res = Solution::find_best_value(arr, target);
         assert_eq!(res, 11361);
